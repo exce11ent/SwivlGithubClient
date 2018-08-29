9e94de97321ea2043307349a11b80800a4d02888
Vitalii Kraiovyi <vitalii.kraiovyi@interpretator.com>
2018-08-29 15:42:56 +0300

    Setup project sctructure. Add User entity

---
+++ SwivlGithubClient/Scenes/Users/UsersViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UsersViewController.swift
+//  SwivlGithubClient
+//
+//  Created by Vitalii Krayovyi on 8/29/18.
+//  Copyright © 2018 Helen Marlen. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UICollectionViewController {
+
+}
+

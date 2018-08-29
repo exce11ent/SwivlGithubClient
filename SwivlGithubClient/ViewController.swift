9ac7d31c10ff80d4382275225b6a9543f109111f
Vitalii Kraiovyi <vitalii.kraiovyi@interpretator.com>
2018-08-29 15:10:58 +0300

    Initial Commit

---
+++ SwivlGithubClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwivlGithubClient
+//
+//  Created by Vitalii Krayovyi on 8/29/18.
+//  Copyright © 2018 Helen Marlen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
